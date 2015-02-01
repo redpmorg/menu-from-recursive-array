205474e8e0dbefcb5413df050532aff9fdb98518
Bastian khalil <bastiankhalil@gmail.com>
2015-02-02 00:22:40 +0100

    Update demo.php

--- demo.php
+++ demo.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<title> Recursive PHP Array  </title>	
-	<link  href="css/style.css" rel="stylesheet" type="text/css">
-</head>
 
-<body>
 <?php
 require_once('function.php');
 require_once('resource.php');
@@ -19,5 +11,3 @@ if (!empty($aMenu)):
 		));
 endif
 ?>
-</body>
-</html>
